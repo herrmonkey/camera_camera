5c95ee1e054a199f7a31cdcc4da0b4ce350e6856
Oscar Cadavid <desarrollo@koherencia.com.co>
2021-10-21 11:57:51 -0500

    add on tapDown Focus

--- lib/src/presentation/widgets/camera_preview.dart
+++ lib/src/presentation/widgets/camera_preview.dart
@@ -45,78 +45,81 @@ class _CameraCameraPreviewState extends State<CameraCameraPreview> {
     return ValueListenableBuilder<CameraCameraStatus>(
       valueListenable: widget.controller.statusNotifier,
       builder: (_, status, __) => status.when(
-          success: (camera) => GestureDetector(
-                onScaleUpdate: (details) {
-                  widget.controller.setZoomLevel(details.scale);
-                },
-                onTapDown: (details) => onViewFinderTap(details, constraints),
-                onDoubleTap: () {
-                  widget.controller.takePhoto();
-                },
-                child: Stack(
-                  children: [
-                    Center(child: widget.controller.buildPreview()),
-                    if (widget.enableZoom)
-                      Positioned(
-                        bottom: 96,
-                        left: 0.0,
-                        right: 0.0,
-                        child: CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.black.withOpacity(0.6),
-                          child: IconButton(
-                            icon: Center(
-                              child: Text(
-                                "${camera.zoom.toStringAsFixed(1)}x",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 12),
-                              ),
-                            ),
-                            onPressed: () {
-                              widget.controller.zoomChange();
-                            },
-                          ),
-                        ),
-                      ),
-                    if (widget.controller.flashModes.length > 1)
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(32.0),
+          success: (camera) =>
+              LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
+                return GestureDetector(
+                  onScaleUpdate: (details) {
+                    widget.controller.setZoomLevel(details.scale);
+                  },
+                  onDoubleTap: () {
+                    widget.controller.takePhoto();
+                  },
+                  onTapDown: (details) => onViewFinderTap(details, constraints),
+                  child: Stack(
+                    children: [
+                      Center(child: widget.controller.buildPreview()),
+                      if (widget.enableZoom)
+                        Positioned(
+                          bottom: 96,
+                          left: 0.0,
+                          right: 0.0,
                           child: CircleAvatar(
                             radius: 20,
                             backgroundColor: Colors.black.withOpacity(0.6),
                             child: IconButton(
+                              icon: Center(
+                                child: Text(
+                                  "${camera.zoom.toStringAsFixed(1)}x",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12),
+                                ),
+                              ),
                               onPressed: () {
-                                widget.controller.changeFlashMode();
+                                widget.controller.zoomChange();
                               },
-                              icon: Icon(
-                                camera.flashModeIcon,
-                                color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      if (widget.controller.flashModes.length > 1)
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: CircleAvatar(
+                              radius: 20,
+                              backgroundColor: Colors.black.withOpacity(0.6),
+                              child: IconButton(
+                                onPressed: () {
+                                  widget.controller.changeFlashMode();
+                                },
+                                icon: Icon(
+                                  camera.flashModeIcon,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 24),
-                        child: InkWell(
-                          onTap: () {
-                            widget.controller.takePhoto();
-                          },
-                          child: CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Color.fromARGB(255,1,171,78),
-                            child: Icon(MfgLabs.camera, color: Colors.white,),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 24),
+                          child: InkWell(
+                            onTap: () {
+                              widget.controller.takePhoto();
+                            },
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundColor: Color.fromARGB(255,1,171,78),
+                              child: Icon(MfgLabs.camera, color: Colors.white,),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
+                    ],
+                  ),
+                );
+              }),
           failure: (message, _) => Container(
                 color: Colors.black,
                 child: Text(message),
@@ -126,13 +129,8 @@ class _CameraCameraPreviewState extends State<CameraCameraPreview> {
               )),
     );
   }
-  
-  void onViewFinderTap(TapDownDetails details, BoxConstraints constraints) {
-    if (_controller == null) {
-      return;
-    }
 
-    final CameraController cameraController = _controller;
+  void onViewFinderTap(TapDownDetails details, BoxConstraints constraints) {
 
     final offset = Offset(
       details.localPosition.dx / constraints.maxWidth,
